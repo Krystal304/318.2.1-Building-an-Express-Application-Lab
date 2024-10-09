220eca942e9d4d8761d9e186a8e5cb19c588a999
Krystal304 <krystalwilliams864@gmail.com>
2024-10-09 14:22:33 -0400

    server

--- script.mjs
+++ script.mjs
@@ -1,2 +1,27 @@
 //Part 1 Routes, Templates and Views
 
+// create server.mjs file
+//npm init -y creates package.json
+// correct package.json
+//npm i express - download express
+//import express at top of page
+//initialize express into a varible
+// listen to express(app) at the BOTTOM
+
+
+// import express at the top of page
+import express from 'express';
+
+
+
+// initialize express into a variable
+const app = express();
+
+// create port
+const PORT = 3000
+
+
+// add listen 
+app.listen(PORT, ()=>{
+    console.log(`Server is running on Port: ${PORT}`);
+});
