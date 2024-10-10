58b998157cd671a19fe22286109d7cb20e33db05
Krystal304 <krystalwilliams864@gmail.com>
2024-10-09 21:58:42 -0400

    middlware

--- server.mjs
+++ server.mjs
@@ -2,6 +2,8 @@
 
 import express from 'express';
 
+import userRouter from './routes/usersRoutes.mjs';
+
 const app = express();
 
 const PORT = 3000;
@@ -9,19 +11,27 @@ const PORT = 3000;
 
 // view engine
 app.set ('view engine', 'ejs')
+// app.use(logger)
 
 
 //routes
-app.get(`/`, (req, res) => {
-    console.log(`Here`)
-    res.render('index', { text: 'World' })
+app.get(`/`, logger, (req, res) => {
+    // console.log(`Here`)
+    res.render('index', { text: 'World' });
     // res.send('Hi')
-})
+});
 
 
 
 app.use('/users', userRouter)
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on PORT: ${PORT}`);
-});
+function logger(req, res, next){
+    console.log(req.originalUrl)
+    next()
+}
+
+
+app.listen (3000)
+// app.listen(PORT, ()=>{
+//     // console.log(`Server is running on PORT: ${PORT}`);
+// });
